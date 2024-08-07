36326db3c3e50b5e15590158ea8e13d26816fd83
HowardLiu <fyisgod@126.com>
2024-08-07 23:25:38 +0800

    test: 路由相关单元测试

--- main.go
+++ main.go
@@ -10,17 +10,20 @@ func main() {
 	r.GET("/", func(c *hgin.Context) {
 		c.HTML(http.StatusOK, "<h1>Hello hgin</h1>")
 	})
+
 	r.GET("/hello", func(c *hgin.Context) {
-		// expect /hello?name=geektutu
+		// expect /hello?name=howard
 		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
 
-	r.POST("/login", func(c *hgin.Context) {
-		c.JSON(http.StatusOK, hgin.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
-		})
+	r.GET("/hello/:name", func(c *hgin.Context) {
+		// expect /hello/howard
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
 	})
 
-	r.Run(":9999")
+	r.GET("/assets/*filepath", func(c *hgin.Context) {
+		c.JSON(http.StatusOK, hgin.H{"filepath": c.Param("filepath")})
+	})
+
+	_ = r.Run(":9999")
 }
