57dfcf1d23064dd40cd25df1ed34814bace60e30
HowardLiu <fyisgod@126.com>
2024-08-07 22:31:29 +0800

    feat: 路由处理

---
+++ hgin/hgin.go
@@ -0,0 +1,41 @@
+package hgin
+
+import (
+	"net/http"
+)
+
+// HandlerFunc 定义请求处理器
+type HandlerFunc func(c *Context)
+
+// Engine 实现 ServeHTTP 接口
+type Engine struct {
+	router *router
+}
+
+// New Engine 的构造器
+func New() *Engine {
+	return &Engine{
+		router: newRouter(),
+	}
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	context := newContext(w, req)
+	engine.router.handle(context)
+}
+
+func (engine *Engine) addRoute(reqMethod, pattern string, handler HandlerFunc) {
+	engine.router.addRoute(reqMethod, pattern, handler)
+}
+
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+// Run 开启一个 HTTP 服务
+func (engine *Engine) Run(addr string) error {
+	return http.ListenAndServe(addr, engine)
+}
---
+++ hgin/router.go
@@ -0,0 +1,93 @@
+package hgin
+
+import (
+	"log"
+	"net/http"
+	"strings"
+)
+
+type router struct {
+	roots    map[string]*node
+	handlers map[string]HandlerFunc
+}
+
+// roots key eg, roots['GET'] roots['POST']
+// handlers key eg, handlers['GET-/p/:lang/doc'], handlers['POST-/p/book']
+
+func newRouter() *router {
+	return &router{
+		roots:    make(map[string]*node),
+		handlers: make(map[string]HandlerFunc),
+	}
+}
+
+// addRoute 添加路由
+func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
+	parts := parsePattern(pattern)
+
+	key := method + "-" + pattern
+	_, ok := r.roots[method]
+	if !ok {
+		r.roots[method] = &node{}
+	}
+	r.roots[method].insert(pattern, parts, 0)
+	r.handlers[key] = handler
+
+	log.Printf("Route %4s - %s", method, pattern)
+}
+
+func (r *router) handle(c *Context) {
+	n, params := r.getRoute(c.Method, c.Path)
+	if n != nil {
+		c.Params = params
+		key := c.Method + "-" + n.pattern
+		r.handlers[key](c)
+	} else {
+		c.String(http.StatusNotFound, "404 NOT FOUND: %s\n", c.Path)
+	}
+}
+
+// getRoute 返回匹配到的路由末节点，还解析了:和*两种匹配符的参数，返回一个 map
+//
+// 例如/p/go/doc匹配到/p/:lang/doc，解析结果为：{lang: "go"}，
+// /static/css/style.css匹配到/static/*filepath，解析结果为{filepath: "css/style.css"}。
+func (r *router) getRoute(method string, path string) (*node, map[string]string) {
+	searchParts := parsePattern(path)
+	params := make(map[string]string)
+	root, ok := r.roots[method]
+	if !ok {
+		return nil, nil
+	}
+
+	// 获取匹配到的路由
+	n := root.search(searchParts, 0)
+	if n != nil {
+		parts := parsePattern(n.pattern)
+		for i, part := range parts {
+			if part[0] == ':' {
+				params[part[1:]] = searchParts[i]
+			}
+			if part[0] == '*' && len(part) > 1 {
+				params[part[1:]] = strings.Join(searchParts[i:], "/")
+				break
+			}
+		}
+		return n, params
+	}
+	return nil, nil
+}
+
+func parsePattern(pattern string) []string {
+	vs := strings.Split(pattern, "/")
+
+	parts := make([]string, 0)
+	for _, item := range vs {
+		if item != "" {
+			parts = append(parts, item)
+			if item[0] == '*' {
+				break
+			}
+		}
+	}
+	return parts
+}
